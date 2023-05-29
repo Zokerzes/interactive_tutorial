3fa0372b5bb10cdc868f81357b766c93f089018a
Zokerzes <yozhik13@gmail.com>
2023-05-29 19:34:00 +0300

    изменил название главной формы

--- interactive_tutorial/MainForm.cs
+++ interactive_tutorial/MainForm.cs
@@ -1,8 +1,8 @@
 namespace interactive_tutorial
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
